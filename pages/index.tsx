9f64a63903963b6c4939b56884c57e3265934f83
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-08 13:08:26 -0600

    Haciendo la peticion de los pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,35 @@
+import { pokeApi } from "../api";
 import { Layout } from "../components/layouts";
-import { Button } from "@nextui-org/react";
-import { NextPage } from "next";
+import { NextPage, GetStaticProps } from "next";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  
+  console.log({props});
+  
+
   return (
     <Layout title="Listado de Pokémons">
-      <Button color='gradient'>Hola mundo</Button>
+      <ul>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+      </ul>
     </Layout>
     
   )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  
+  const { data } = await pokeApi.get('/pokemon?limit=151')
+  
+  return{
+    props: {
+      pokemons: data.results
+    }
+  }
 }
 
 export default HomePage;
