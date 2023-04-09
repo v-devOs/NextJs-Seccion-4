a1239ddaab0ed33318158763a204a70ab7175f06
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-09 13:26:43 -0600

    Generando las paginas estaticas de pokemons

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,47 +1,42 @@
-import { Layout } from "</components/layouts>";
+import { pokeApi } from "</api>";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import { useRouter } from "next/router";
+import { Layout } from "</components/layouts>";
+import { Pokemmon } from "</interfaces>";
 
 interface Props{
-  // pokemon: any
-  id: string
-  name: string
+  pokemon: Pokemmon
+  
 }
 
-const PokemonPage: NextPage<Props> = ({ id , name }) => {
-
-  const router = useRouter();
-  console.log(router.query);
-  
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
+    
   return (
     <Layout>
-      <h1>Hola mundo</h1>
+      <h1>{pokemon.name}</h1>
     </Layout>
   )
 }
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
+  const pokemons151 = [...Array(151)].map( (value, index) => `${index + 1}` )
+  
   return {
-    paths: [
-      {
-        params: {
-          id: '1'
-        }
-      }
-    ],
+    paths: pokemons151.map( id => ({
+      params : { id } 
+    })),
     fallback: false
   }
 }
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   
- 
+  const { id } = params as { id : string};
+  const { data } = await pokeApi.get<Pokemmon>(`pokemon/${id}`)
 
   return{
     props: {
-      id: 1,
-      name: 'bulbasaur'
+      pokemon: data
     }
   }
 }
