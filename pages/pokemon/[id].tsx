7b22d7d8fdbb3051e74807bdca010ce19e68ed43
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-11 13:21:59 -0600

    ISG e ISR

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -114,20 +114,30 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemons151.map( id => ({
       params : { id } 
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   
   const { id } = params as { id : string};
+  const pokemon =  await getPokemonInfo( id );
+
+  if( !pokemon ){
+    return {
+      redirect: {
+        destination : '/',
+        permanent: false,
+      }
+    }
+  }
 
   return{
     props: {
-      pokemon: await getPokemonInfo( id )
-    }
+      pokemon
+    },
+    revalidate: 86400,
   }
 }
-
 
 export default PokemonPage;
--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,12 +2,19 @@ import { pokeApi } from "</api>"
 import { Pokemmon } from "</interfaces>"
 
 export const getPokemonInfo = async( keyPokemon: string ) => {
-  const { data } = await pokeApi.get<Pokemmon>(`pokemon/${keyPokemon}`)
 
-  return  {
-    name: data.name,
-    id: data.id,
-    sprites: data.sprites
+  try {
+    const { data } = await pokeApi.get<Pokemmon>(`pokemon/${keyPokemon}`)
+
+    return  {
+      name: data.name,
+      id: data.id,
+      sprites: data.sprites
+    }
+  } catch (error) {
+    return null;
   }
+
+  
  
 }
