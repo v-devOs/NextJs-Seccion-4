98230f3831c4c71c15b73a79b200621b0b9a34a5
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-11 03:20:28 -0600

    Añadiendo metatags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,11 +1,13 @@
-import Head from "next/head"
 import { FC, ReactNode } from "react"
+import Head from "next/head"
 import { Navbar } from "../ui"
 
 interface PropsLayout {
   children: ReactNode,
   title?: string
 }
+
+const origin = typeof window === 'undefined' ? '' : window.location.origin
 
 export const Layout: FC<PropsLayout> = ({ children, title }) => {
   return (
@@ -15,6 +17,9 @@ export const Layout: FC<PropsLayout> = ({ children, title }) => {
         <meta name="author" content="Uriel Emiliano"/>
         <meta name="description" content={`Informacion sobre el pokémon ${title}`}/>
         <meta name="keywords" content="xxxx, pokemon, pokedex"/>
+        <meta property="og:title" content={`Informacion sobre ${title}`} />
+        <meta property="og:description" content={`Esta es la pagina sobre ${title}`} />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
 
       <Navbar/>
